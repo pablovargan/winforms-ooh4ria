3f1ce04eae72bab3a27a70803fa13114988f8fac
Hectormm <hectormm@gmail.com>
2014-03-23 15:46:11 +0100

    Pantalla principal con instancias unicas por pestaña

--- LimpiezasPalmeralForms/PantallaPrincipal.cs
+++ LimpiezasPalmeralForms/PantallaPrincipal.cs
@@ -20,32 +20,87 @@ namespace LimpiezasPalmeralForms
 
         private void cliente_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is PantallaCliente)
+                {
+                    form.Activate();
+                    form.WindowState = FormWindowState.Maximized;
+                    return;
+                }
+            }
             PantallaCliente cli = new PantallaCliente() { MdiParent = this };
             cli.Show();
+            cli.WindowState = FormWindowState.Maximized;
+
         }
 
         private void instalacion_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is PantallaInstalacion)
+                {
+                    form.Activate();
+                    form.WindowState = FormWindowState.Maximized;
+                    return;
+                }
+            }
             PantallaInstalacion inst = new PantallaInstalacion() { MdiParent = this };
             inst.Show();
+            inst.WindowState = FormWindowState.Maximized;
+
         }
 
         private void producto_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is PantallaProducto)
+                {
+                    form.Activate();
+                    form.WindowState = FormWindowState.Maximized;
+                    return;
+                }
+            }
             PantallaProducto prod = new PantallaProducto() { MdiParent = this };
             prod.Show();
+            prod.WindowState = FormWindowState.Maximized;
         }
 
         private void proveedor_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is PantallaProveedor)
+                {
+                    form.Activate();
+                    form.WindowState = FormWindowState.Maximized;
+                    return;
+                }
+            }
             PantallaProveedor prov = new PantallaProveedor() { MdiParent = this };
             prov.Show();
+            prov.WindowState = FormWindowState.Maximized;
+           // this.LayoutMdi(MdiLayout.TileVertical);
         }
 
         private void trabajador_Click(object sender, EventArgs e)
         {
+            foreach (Form form in Application.OpenForms)
+            {
+                if (form is PantallaTrabajador)
+                {
+                    form.Activate();
+                    form.WindowState = FormWindowState.Maximized;
+                    return;
+                }
+            }
+                    
             PantallaTrabajador trab = new PantallaTrabajador() { MdiParent = this };
             trab.Show();
+            trab.WindowState = FormWindowState.Maximized;
+
         }
     }
 }
