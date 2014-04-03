ef7eb89cf9eae119270185fd0aa12e783c774169
Víctor Vives <vvb7@alu.ua.es>
2014-04-03 17:44:07 +0200

    Modificación interfaz pantalla instalación

--- LimpiezasPalmeralForms/Instalacion/PantallaInstalacion.Designer.cs
+++ LimpiezasPalmeralForms/Instalacion/PantallaInstalacion.Designer.cs
@@ -34,6 +34,10 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.Instalación.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.instalacionGrid)).BeginInit();
             this.groupBox2.SuspendLayout();
@@ -91,6 +95,10 @@
             // 
             this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
             this.groupBox2.Controls.Add(this.button1);
             this.groupBox2.Location = new System.Drawing.Point(840, 12);
             this.groupBox2.Name = "groupBox2";
@@ -108,6 +116,42 @@
             this.button1.Text = "Dar de alta";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.AltaInstalacion_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(6, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(165, 57);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Dar de baja";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 82);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(165, 57);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Editar";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(165, 57);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Gestionar trabajadores";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(6, 271);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(165, 57);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Consultar trabajadores";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // PantallaInstalacion
             // 
@@ -135,5 +179,9 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.DataGridView instalacionGrid;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
     }
 }
