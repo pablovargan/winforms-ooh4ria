6047db6ec93d03b584a14ff317a2bc1683f9a411
Hectormm <hectormm@gmail.com>
2014-05-16 12:41:35 +0200

    Creación de backups de la BD
    Ya se crean los backups de la base de datos en D:\  el respaldo no
    funciona ya que se queda la conexión de sqlserver abierta y por tanto no
    podemos restaurar.

--- LimpiezasPalmeralForms/CopiaSeguridad/PantallaBackup.cs
+++ LimpiezasPalmeralForms/CopiaSeguridad/PantallaBackup.cs
@@ -17,7 +17,7 @@ namespace LimpiezasPalmeralForms.Backup
 {
     public partial class PantallaBackup : Form
     {
-
+        int contador = 0;
 
 
         public PantallaBackup()
@@ -25,29 +25,96 @@ namespace LimpiezasPalmeralForms.Backup
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
+        private string comprobarNombre(string nombre){
+            if(File.Exists(@"d:\" + nombre + "-" + contador +".bak")){
+                contador++;
+                nombre = comprobarNombre(nombre);
+            }
+            return nombre;
+        }
 
-             //poner cursor de relojito mintras respalda
-             Cursor.Current = Cursors.WaitCursor;
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //poner cursor de relojito mientras respalda
+            Cursor.Current = Cursors.WaitCursor;
 
 
             //esto puede ser un método aparte de conexion a la base de datos-----------
 
-            SqlConnection connect = new SqlConnection(@"Server=(local)\SQLEXPRESS; database=master; integrated security=yes");
+            SqlConnection connect = new SqlConnection(@"Server=(local)\SQLEXPRESS; database=PalmeralGenNHibernate; integrated security=yes");
             connect.Open();
             //-------------------------------------------------------------------------
 
             //esto puede ser un método aparte para ejecutar comandos SQL---------------
+            string nombre = "resp" + DateTime.Now.Day.ToString() + "-" + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Year.ToString();
+
+            nombre = comprobarNombre(nombre) + "-" + contador;
+
             SqlCommand command;
-            command = new SqlCommand(@"backup database PalmeralGenNHibernate to disk ='c:\resp.bak' with init,stats=10", connect);
+            command = new SqlCommand(@"backup database PalmeralGenNHibernate to disk ='d:\" + nombre + ".bak' with init,stats=10", connect);
             command.ExecuteNonQuery();
             //-------------------------------------------------------------------------
 
             connect.Close();
 
-            MessageBox.Show("El Respaldo de la base de datos fue realizado satisfactoriamente", "Respaldo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //Añade la copia al datagrid
+            DataGridViewRow row = (DataGridViewRow)dataGridViewCopiasSeguridad.Rows[0].Clone();
+            row.Cells[0].Value = DateTime.Now.ToString();
+            row.Cells[1].Value = nombre;
+            dataGridViewCopiasSeguridad.Rows.Add(row);
+
+
+            MessageBox.Show("El Respaldo de la base de datos fue realizada satisfactoriamente", "Respaldo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+   
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            //poner cursor de relojito
+            Cursor.Current = Cursors.WaitCursor;
+
+            string fichero = dataGridViewCopiasSeguridad.SelectedRows[0].Cells[1].Value.ToString();
+
+              
+
+            try
+            {
+                if (File.Exists(@"d:\" + fichero))
+                {
+                    if (MessageBox.Show("¿Está seguro de restaurar?", "Respaldo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                    {
+                        //esto puede ser un método aparte de conexion a la base de datos-----------
+
+                        SqlConnection connect = new SqlConnection(@"Server=(local)\SQLEXPRESS; database=PalmeralGenNHibernate; integrated security=yes");
+                        connect.Open();
+                        //--------------------------------------------------------------------------
+
+                        //esto puede ser un método aparte para ejecutar comandos SQL----------------
+                        SqlCommand command;
+		                command = new SqlCommand("use master", connect);
+  		                command.ExecuteNonQuery();
+                        command = new SqlCommand(@"restore database PalmeralGenNHibernate from disk = 'd:\" + fichero +"'", connect);
+                        command.ExecuteNonQuery();
+                        //--------------------------------------------------------------------------
+                        connect.Close();
+
+                        MessageBox.Show("Se ha restaurado la base de datos", "Restauración", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else
+                    MessageBox.Show(@"No haz hecho ningun respaldo anteriormente (o no está en la ruta correcta)", "Restauracion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+
+        }
+
 
 
         
